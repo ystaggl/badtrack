0d93a2b4beca10026c0557f667c0bdb11fda4b59
Jacky-Poon5929 <jacky.p5929@gmail.com>
2023-08-11 00:43:01 +1000

    Works on Fresh install of Ubuntu 1. sudo dpkg =i badtrack.deb 2. history folder appears in /var/lib/badtrack/history

--- build_badtrack.sh
+++ build_badtrack.sh
@@ -12,12 +12,16 @@ mkdir -p "$APP_PATH"
 mkdir -p "$APP_PATH/badtrack/DEBIAN"
 mkdir -p "$APP_PATH/badtrack/usr/local/bin"
 mkdir -p "$APP_PATH/badtrack/usr/local/share/badtrack"
+mkdir -p "$APP_PATH/badtrack/usr/local/bin/badtrack"
 
-# Create the history directory in the current working directory
-mkdir -p "$APP_PATH/history"
+# Make the pre-installation script executable
+chmod +x "\$APP_PATH/badtrack/DEBIAN/preinst"
 
 # Copy the main.py file to the badtrack directory and make it executable
 cp main.py "$APP_PATH/badtrack/usr/local/bin/badtrack"
+cp main.py "$APP_PATH/main.py"
+cp main.py "$APP_PATH/badtrack/usr/local/bin/badtrack/main.py"
+chmod +x "$APP_PATH/badtrack/usr/local/bin/badtrack/main.py"
 chmod +x "$APP_PATH/badtrack/usr/local/bin/badtrack"
 
 # Create the control file
@@ -31,11 +35,14 @@ Installed-Size: 1024
 Maintainer: Your Name <your-email@example.com>
 Description: Badtrack is a sample package" > "$APP_PATH/badtrack/DEBIAN/control"
 
+
 # Create the post-installation script
-echo "#!/bin/sh
-systemctl enable badtrack
-systemctl start badtrack" > "$APP_PATH/badtrack/DEBIAN/postinst"
-chmod +x "$APP_PATH/badtrack/DEBIAN/postinst"
+echo "#!/bin/bash
+
+# Create the directory with proper permissions
+mkdir -p "/var/lib/badtrack/history"
+chown -R $(whoami):$(whoami) "/var/lib/badtrack/history"
+chmod 755 "/var/lib/badtrack/history"
 
 # Create the systemd service file
 cat <<EOL | sudo tee /etc/systemd/system/badtrack.service
@@ -46,24 +53,28 @@ After=network.target
 [Service]
 Type=simple
 User=$(whoami)
-WorkingDirectory=$APP_PATH
-ExecStart=/usr/bin/python3 $APP_PATH/badtrack/main.py  # Run main.py directly
+WorkingDirectory=/usr/local/bin/badtrack
+ExecStart=/usr/bin/python3 /usr/local/bin/badtrack/main.py
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
-# Change ownership of the badtrack directory to the current user
-sudo chown -R $(whoami):$(whoami) "$APP_PATH/badtrack"
+# Set the correct permissions for the service file
+sudo chmod 644 /etc/systemd/system/badtrack.service
+
+# Reload the systemd daemon to recognize the new service file
+sudo systemctl daemon-reload
+
+# Enable and start the service
+sudo systemctl enable badtrack
+sudo systemctl start badtrack" > "$APP_PATH/badtrack/DEBIAN/postinst"
+
+# Make the post-installation script executable
+chmod +x "$APP_PATH/badtrack/DEBIAN/postinst"
 
 # Build the Debian package
 dpkg-deb --build "$APP_PATH/badtrack"
-
-# Install the Debian package
-sudo dpkg -i "$APP_PATH/badtrack.deb"
-
-# Reload systemd daemon
-sudo systemctl daemon-reload
 
 # Print the status
 sudo systemctl status badtrack
